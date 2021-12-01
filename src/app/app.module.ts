ccaaccbc7ad8f681c3facf7d51c27d398fe4254f
“Gabriel <gabrieljoseruizmartinez@gmail.com>
2021-12-01 16:23:41 -0500

    Segunda version, Problema 2, estilo,orden, codigo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { App1Component } from './app1/app1.component';
+import { App2Component } from './app2/app2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    App1Component,
+    App2Component
   ],
   imports: [
     BrowserModule,
